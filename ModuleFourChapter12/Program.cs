80a22a8467e391c42a836f4878d3c9a8ba2ff6b0
Bou <Bou@DESKTOP-67EENV6>
2016-11-16 20:27:18 +0200

    Add project files.

---
+++ ModuleFourChapter12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleFourChapter12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////
+            DeclareImplicitVars();
+            ///////
+            addNumbers nums1000 = new addNumbers();
+            List<randomNumbers> Tlist = nums1000.returnList();
+            foreach (var x in Tlist)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+        static void DeclareImplicitVars()
+        {
+            // Implicitly typed local variables.
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
+            // Print out the underlying type.
+            Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+            Console.WriteLine("myBool is a: {0}", myBool.GetType().Name);
+            Console.WriteLine("myString is a: {0}", myString.GetType().Name);
+        }
+    }
+}
---
+++ ModuleFourChapter12/randomNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleFourChapter12
+{
+    public class randomNumbers
+    {
+        public int randomNum { get; set; }
+    }
+    public class addNumbers
+    {
+        private List<randomNumbers> listNumbers;
+        public addNumbers()
+        {
+            listNumbers = new List<randomNumbers>();
+            for (int i = 1; i < 10; i++)
+            {
+                listNumbers.Add(new randomNumbers() { randomNum = i });
+            }
+        }
+        public List<randomNumbers> returnList()
+        {            
+            return listNumbers;
+        }
+    }
+}
